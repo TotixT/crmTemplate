f86c44e76960f508e74b9fe889543b5fd9f4608a
TotixT <santiagolopezgarcia22@gmail.com>
2023-05-25 09:21:55 -0500

    Eliminado

--- Estudiantes/database.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE campus;
-CREATE TABLE campers(
-    id INT primary key AUTO_INCREMENT,
-    NOMBRES VARCHAR (50) NOT NULL,
-    DIRECCION VARCHAR (40) NOT NULL,
-    LOGROS VARCHAR (60) NOT NULL
-);
-
-INSERT INTO `campers` values ('Santiago','Calle 111','Tuve sexo');
